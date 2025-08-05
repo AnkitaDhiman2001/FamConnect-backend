1f9738944c2eaf3e435d809448926080c462bb63
AnkitaDhiman2001 <161723895+AnkitaDhiman2001@users.noreply.github.com>
2025-08-05 17:40:28 +0530

    Updated src

---
+++ src/migrations/20250805110848-create-messages.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('messages', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      room_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('messages');
+  },
+};
